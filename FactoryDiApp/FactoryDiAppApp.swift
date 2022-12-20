1e3744d44ad3d1b4f578b2013fc80a72503a0b1b
Egor Ledkov <icemanea@gmail.com>
2022-12-20 21:45:58 +0300

    Initial Commit

---
+++ FactoryDiApp/FactoryDiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactoryDiAppApp.swift
+//  FactoryDiApp
+//
+//  Created by Egor Ledkov on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct FactoryDiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
